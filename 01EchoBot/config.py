134d3bebe8545f864e500892987f6643f8e0c14b
AceTheNetwork <42460629+AceTheNetwork@users.noreply.github.com>
2021-07-02 04:47:36 -0500

    Update config.py

--- 01EchoBot/config.py
+++ 01EchoBot/config.py
@@ -8,5 +8,5 @@ class DefaultConfig:
     """ Bot Configuration """
 
     PORT = 3978
-    APP_ID = os.environ.get("MicrosoftAppId", "b54bf997-b017-41ab-92ca-3e516cf18dfb")
-    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "Aev_AJ96~XU-53Zj26rVaFN.rh_t87Wqto")
+    APP_ID = os.environ.get("MicrosoftAppId", "5a40ed0e-459b-41f4-8ca1-940d8da01ce9")
+    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "4w4HnA-_g~KSpI5vN~.2To.qE82VOjAiqR")
